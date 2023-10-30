cee461afc076832634da9da2ff1a24da558e22b8
ChandruMonica2705 <monicamahalingam27@gmail.com>
2023-10-30 08:52:57 +0530

    answers updated

--- script.js
+++ script.js
@@ -45,13 +45,13 @@ var title=function (str) {
   }
   console.log(title("moni chandru diya"))
   // IIFE : 
-//   (function (str) {
-//     str = str.toLowerCase().split(' ');
-//     for (var i = 0; i < str.length; i++) {
-//       str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-//     } 
-//     return str.join(' ');
-//   })("diyakshara is my daughter");
+  (function (str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    } 
+    return str.join(' ');
+  })("diyakshara is my daughter");
   // Arrow Function 
   var titleCase = (str) => {
     str = str.toLowerCase().split(' ');
@@ -64,11 +64,157 @@ var title=function (str) {
 
  // =>Q3.Sum of all numbers in an array
  // Anonymous  : 
- var sum = []; 
- var add=function(array){
-    for(var i = 0 ; i< array.length ; i++){
-       sum = sum + array[i];
-     }
-     return sum;
-  }
-console.log(add[1,2,3,4])
+  function(array){
+  var sum = 0;
+  for(var i = 0 ; i< array.length ; i++){
+     sum = sum + array[i];
+   }
+   return sum;
+}
+// IIFE :       
+(function(array){
+var sum = 0;
+  for(var i = 0 ; i< array.length ; i++){
+     sum = sum + array[i];
+   }
+   return sum;
+})([1,2,3,4])
+// Arrow:       
+sum = (array)=>{
+var sum = 0;
+  for(var i = 0 ; i< array.length ; i++){
+     sum = sum + array[i];
+   }
+   return sum;
+   }
+
+//=>Q4.Return all the prime numbers in an array
+    //Anonymous 
+    function(numArray){
+                      numArray = numArray.filter((number) => {
+                        for (var i = 2; i <= Math.sqrt(number); i++) {
+                          if (number % i === 0) return false;
+                        }
+                        return true;
+                      });
+                      console.log(numArray);
+                  }
+   //IIFE 
+                   (  
+                   function(numArray){
+                      numArray = numArray.filter((number) => {
+                        for (var i = 2; i <= Math.sqrt(number); i++) {
+                          if (number % i === 0) return false;
+                        }
+                        return true;
+                      });
+                      console.log(numArray);
+                  })([1,2,3,4])
+    //Arrow 
+                primeNumber = (numArray) => {
+                      numArray = numArray.filter((number) => {
+                        for (var i = 2; i <= Math.sqrt(number); i++) {
+                          if (number % i === 0) return false;
+                        }
+                        return true;
+                      });
+                      console.log(numArray);
+                  }
+//=>Q5.Return all the palindromes in an array
+function isPalindrome(N)
+    {
+        let str = "" + N;
+        let len = str.length;
+        for (let i = 0; i < parseInt(len / 2, 10); i++)
+        {
+            if (str[i] != str[len - 1 - i ])
+                return false;
+        }
+        return true;
+    }
+  //Anonymous   
+  function (arr, n)
+    {
+        // Traversing each element of the array
+        // and check if it is palindrome or not
+        for (let i = 0; i < n; i++)
+        {
+            let ans = isPalindrome(arr[i]);
+            if (ans == false)
+                return false;
+        }
+        return true;
+    }
+    //IIFE : 
+    (  function (arr, n)
+        {
+            // Traversing each element of the array
+            // and check if it is palindrome or not
+            for (let i = 0; i < n; i++)
+            {
+                let ans = isPalindrome(arr[i]);
+                if (ans == false)
+                    return false;
+            }
+            return true;
+        })([1,2,3] , 3)
+//Arrow : 
+Palindrome = (arr, n) =>
+    {
+        // Traversing each element of the array
+        // and check if it is palindrome or not
+        for (let i = 0; i < n; i++)
+        {
+            let ans = isPalindrome(arr[i]);
+            if (ans == false)
+                return false;
+        }
+        return true;
+    }
+  //=>Q7.Remove duplicates from an Array
+    //Anonymous 
+    function(array){
+                            let dup = [...new Set(array)];
+                            console.log(dup);
+                          }
+    //IIFE            
+    (function(array){
+      let dup = [...new Set(array)];
+      console.log(dup);
+       })([1,1,2,3,4])
+                           
+//=>Q8.Rotate an array by K times
+function reverse(array , li , ri){
+        while(li < ri){
+             int temp = a[li];
+             a[li]= a[ri];
+             a[ri] = temp;
+             
+             li++;
+             ri--;
+           }
+       }
+//Anonymous
+        function(array , k){
+                                 k = k % a.length;
+                                   if(k < 0){
+                                     k += a.length;
+                                   }
+       
+                                   reverse(a, 0, a.length - k - 1);
+                                   reverse(a, a.length - k, a.length - 1);
+                                   reverse(a, 0, a.length - 1);
+                                 }
+                                 
+//IIFE :   
+(function(array , k){
+                                 k = k % a.length;
+                                   if(k < 0){
+                                     k += a.length;
+                                   }
+       
+                                   reverse(a, 0, a.length - k - 1);
+                                   reverse(a, a.length - k, a.length - 1);
+                                   reverse(a, 0, a.length - 1);
+                                 })([1,2,3,4] , 2)
+             
