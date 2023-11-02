bc2e336eb27127d5890c910449dd06eb2bff025f
ChandruMonica2705 <monicamahalingam27@gmail.com>
2023-11-02 23:05:47 +0530

    changes done

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var a = function (arr){
   return result;
 }
 console.log(a([1,2,3,4,5,6,7,8,9,10]));
-// IIFE :       
+// // IIFE :       
 var result1 = [];
 (function (arr){
   for(var i=0;i<arr.length;i=i+1){
@@ -18,9 +18,9 @@ var result1 = [];
         result1.push(arr[i]);
     }
   }
-  console.log(result1) 
+  console.log(result1) ;
 })([1,2,3,4,5,6,7,8,9,10])
-// Arrow Function 
+// // Arrow Function 
 var result = [];
 var odd =(arr)=>{
   for(var i=0;i<arr.length;i=i+1){
@@ -31,190 +31,183 @@ var odd =(arr)=>{
   return result;
 }
 odd([15,18,23,14,16,24]);
-console.log(result)  
+console.log(result);  
 
-// =>Q2.Convert all the strings to title caps in a string array
-// Anonymous  :  
+// // =>Q2.Convert all the strings to title caps in a string array
+// // Anonymous  :  
 var title=function (str) {
-    str = str.toLowerCase().split(' ');
+    str = str.toLowerCase().split(" ");
     for (var i = 0; i < str.length; i++) {
-      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+      str[i] = str[i].charAt(0).toUpperCase()+str[i].slice(1); 
     } 
-    
-    return str.join(' ');
+    return str.join(" ");
   }
-  console.log(title("moni chandru diya"))
-  // IIFE : 
-  (function (str) {
-    str = str.toLowerCase().split(' ');
+  console.log(title("moNi chaNdru diYa"));
+//   // IIFE : 
+(function (str1) {
+  str1 = str1.toLowerCase().split(" ");
+  for (var i = 0; i < str1.length; i++) {
+    str1[i] = str1[i].charAt(0).toUpperCase()+str1[i].slice(1); 
+  } 
+  console.log(str1.join(" "));
+})("moNi chaNdru diYa")
+//   // Arrow Function 
+var titleCase = (str) => {
+    str = str.toLowerCase().split(" ");
     for (var i = 0; i < str.length; i++) {
       str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
     } 
-    return str.join(' ');
-  })("diyakshara is my daughter");
-  // Arrow Function 
-  var titleCase = (str) => {
-    str = str.toLowerCase().split(' ');
-    for (var i = 0; i < str.length; i++) {
-      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-    } 
-    return str.join(' ');
+    return str.join(" ");
   }            
- console.log(titleCase("diyakshara is my daughter"))
+ console.log(titleCase("diyakshara is my daughter"));
 
- // =>Q3.Sum of all numbers in an array
- // Anonymous  : 
-  function(array){
-  var sum = 0;
-  for(var i = 0 ; i< array.length ; i++){
-     sum = sum + array[i];
-   }
-   return sum;
-}
-// IIFE :       
-(function(array){
+//  // =>Q3.Sum of all numbers in an array
+//  // Anonymous  : 
+var sum=function(arr){
+  var num=0;
+  for(var i=0;i<arr.length;i++)
+  {
+    num=num+arr[i];
+  }
+  return num;}
+  console.log(sum([1,2,3]));
+// // IIFE :       
+ (function(array){
 var sum = 0;
   for(var i = 0 ; i< array.length ; i++){
      sum = sum + array[i];
    }
-   return sum;
+   console.log(sum);
 })([1,2,3,4])
-// Arrow:       
-sum = (array)=>{
-var sum = 0;
-  for(var i = 0 ; i< array.length ; i++){
-     sum = sum + array[i];
-   }
-   return sum;
-   }
+// // Arrow:       
+var sum=(arr)=>{
+  var num=0;
+  for(var i=0;i<arr.length;i++)
+  {
+    num=num+arr[i];
+  }
+  return num;}
+  console.log(sum([1,2,3,4,5]));
 
-//=>Q4.Return all the prime numbers in an array
-    //Anonymous 
-    function(numArray){
-                      numArray = numArray.filter((number) => {
-                        for (var i = 2; i <= Math.sqrt(number); i++) {
-                          if (number % i === 0) return false;
-                        }
-                        return true;
-                      });
-                      console.log(numArray);
-                  }
-   //IIFE 
-                   (  
-                   function(numArray){
-                      numArray = numArray.filter((number) => {
-                        for (var i = 2; i <= Math.sqrt(number); i++) {
-                          if (number % i === 0) return false;
-                        }
-                        return true;
-                      });
-                      console.log(numArray);
-                  })([1,2,3,4])
-    //Arrow 
-                primeNumber = (numArray) => {
-                      numArray = numArray.filter((number) => {
-                        for (var i = 2; i <= Math.sqrt(number); i++) {
-                          if (number % i === 0) return false;
-                        }
-                        return true;
-                      });
-                      console.log(numArray);
-                  }
+// //=>Q4.Return all the prime numbers in an array
+//     //Anonymous 
+//     function(numArray){
+//                       numArray = numArray.filter((number) => {
+//                         for (var i = 2; i <= Math.sqrt(number); i++) {
+//                           if (number % i === 0) return false;
+//                         }
+//                         return true;
+//                       });
+//                       console.log(numArray);
+//                   }
+//    //IIFE 
+//                    (  
+//                    function(numArray){
+//                       numArray = numArray.filter((number) => {
+//                         for (var i = 2; i <= Math.sqrt(number); i++) {
+//                           if (number % i === 0) return false;
+//                         }
+//                         return true;
+//                       });
+//                       console.log(numArray);
+//                   })([1,2,3,4])
+//     //Arrow 
+//                 primeNumber = (numArray) => {
+//                       numArray = numArray.filter((number) => {
+//                         for (var i = 2; i <= Math.sqrt(number); i++) {
+//                           if (number % i === 0) return false;
+//                         }
+//                         return true;
+//                       });
+//                       console.log(numArray);
+                  // }
 //=>Q5.Return all the palindromes in an array
-function isPalindrome(N)
-    {
-        let str = "" + N;
-        let len = str.length;
-        for (let i = 0; i < parseInt(len / 2, 10); i++)
-        {
-            if (str[i] != str[len - 1 - i ])
-                return false;
-        }
-        return true;
-    }
-  //Anonymous   
-  function (arr, n)
-    {
-        // Traversing each element of the array
-        // and check if it is palindrome or not
-        for (let i = 0; i < n; i++)
-        {
-            let ans = isPalindrome(arr[i]);
-            if (ans == false)
-                return false;
-        }
-        return true;
-    }
-    //IIFE : 
-    (  function (arr, n)
-        {
-            // Traversing each element of the array
-            // and check if it is palindrome or not
-            for (let i = 0; i < n; i++)
-            {
-                let ans = isPalindrome(arr[i]);
-                if (ans == false)
-                    return false;
-            }
-            return true;
-        })([1,2,3] , 3)
-//Arrow : 
-Palindrome = (arr, n) =>
-    {
-        // Traversing each element of the array
-        // and check if it is palindrome or not
-        for (let i = 0; i < n; i++)
-        {
-            let ans = isPalindrome(arr[i]);
-            if (ans == false)
-                return false;
-        }
-        return true;
-    }
-  //=>Q7.Remove duplicates from an Array
-    //Anonymous 
-    function(array){
-                            let dup = [...new Set(array)];
-                            console.log(dup);
-                          }
-    //IIFE            
-    (function(array){
-      let dup = [...new Set(array)];
-      console.log(dup);
-       })([1,1,2,3,4])
+//Anonymous 
+var Palindrome=function(arr)
+{
+let res = [];
+for (let i=0;i<arr.length;i++)
+{
+  var data=arr[i].split("").reverse().join("");
+  if(data===arr[i]){
+  res.push(arr[i])
+  }  
+}
+return res;}
+console.log(Palindrome (['mom','dad','data']));
+//IIFE : 
+(function(arr){
+let res1 = [];
+for (let i=0;i<arr.length;i++)
+{
+  var data1=arr[i].split("").reverse().join("");
+  if(data1===arr[i]){
+  res1.push(arr[i]);
+  }  
+}
+console.log(res1);
+})(['mom','dad','data',"madam"])
+// //Arrow : 
+ var arrow=(arr)=>{
+let res2 = [];
+for (let i=0;i<arr.length;i++)
+{
+  var data2=arr[i].split("").reverse().join("");
+  if(data2===arr[i]){
+  res2.push(arr[i]);
+  }  
+}
+return res2;
+ }
+console.log(arrow(["data","madam","dad"]));
+
+
+
+//   //=>Q7.Remove duplicates from an Array
+
+//     //Anonymous 
+//     function(array){
+//                             let dup = [...new Set(array)];
+//                             console.log(dup);
+//                           }
+//     //IIFE            
+//     (function(array){
+//       let dup = [...new Set(array)];
+//       console.log(dup);
+//        })([1,1,2,3,4])
                            
-//=>Q8.Rotate an array by K times
-function reverse(array , li , ri){
-        while(li < ri){
-             int temp = a[li];
-             a[li]= a[ri];
-             a[ri] = temp;
+// //=>Q8.Rotate an array by K times
+// function reverse(array , li , ri){
+//         while(li < ri){
+//              int temp = a[li];
+//              a[li]= a[ri];
+//              a[ri] = temp;
              
-             li++;
-             ri--;
-           }
-       }
-//Anonymous
-        function(array , k){
-                                 k = k % a.length;
-                                   if(k < 0){
-                                     k += a.length;
-                                   }
+//              li++;
+//              ri--;
+//            }
+//        }
+// //Anonymous
+//         function(array , k){
+//                                  k = k % a.length;
+//                                    if(k < 0){
+//                                      k += a.length;
+//                                    }
        
-                                   reverse(a, 0, a.length - k - 1);
-                                   reverse(a, a.length - k, a.length - 1);
-                                   reverse(a, 0, a.length - 1);
-                                 }
+//                                    reverse(a, 0, a.length - k - 1);
+//                                    reverse(a, a.length - k, a.length - 1);
+//                                    reverse(a, 0, a.length - 1);
+//                                  }
                                  
-//IIFE :   
-(function(array , k){
-                                 k = k % a.length;
-                                   if(k < 0){
-                                     k += a.length;
-                                   }
+// //IIFE :   
+// (function(array , k){
+//                                  k = k % a.length;
+//                                    if(k < 0){
+//                                      k += a.length;
+//                                    }
        
-                                   reverse(a, 0, a.length - k - 1);
-                                   reverse(a, a.length - k, a.length - 1);
-                                   reverse(a, 0, a.length - 1);
-                                 })([1,2,3,4] , 2)
+//                                    reverse(a, 0, a.length - k - 1);
+//                                    reverse(a, a.length - k, a.length - 1);
+//                                    reverse(a, 0, a.length - 1);
+//                                  })([1,2,3,4] , 2)
              
